1542974f34e0632f880728fce9c3a8bbe2637ee4
0456franco <franco.petitfour001@gmail.com>
2025-10-02 08:07:05 -0400

    In this commit I'll be poiting out the changes made to the extract_images_urls.sh script. The major changes are due to the fact that the necessity arose to mark a starting point from the product IDs in our shopfy store. I've also added some additional logging and log formatting.
    1. Added a console log for stating that an output file already exists. Script was exiting without a log in the UI.
    2. I've declared a FROM_ID variable. This can be set as a starting point in your Shopify Product listing.
    3. Added a conditional check to see if the FROM_ID is greater than 0. If it's greater than 0, we'll add the query to the GraphQL Selector, otherwise we'll get all the products.
    4. In line 53, we needed some additional escapes.
    5. Added some logging to record the fact that the script inserts the first from product outside of the main execution loop. This happens because we need a product cursor to begin our operations from.
     Instead of querying for that product again, we'll just record it when we pull its cursor.
    6. Right after querying the first product, we also want to increment the current_product and curren_batch_product iteratators.
    7. Now we check if the TOTAL_PRODUCTS is less than the PRODUCTS_PER_BATCH. The PRODUCTS_PER_BATCH is set at 250 by default since this is how many products Shopify lets us retrieve per query. If the total products is less than 250, we'll want to update the PRODUCTS_PER_BATCH to the number of TOTAL_PRODUCTS so that we can finalize our execution loop.
    8. Counters start from 1 since we don't rely on zero-based numbering anywhere in our script.
    9. We were also incrementing the current_batch_product and current_product before checking if the batch should end. This was causing the script to skip the last product of each batch.

--- extract_images_urls.sh
+++ extract_images_urls.sh
@@ -18,8 +18,13 @@ fi
 # Ensure OUTPUT_FILE does not exist
 if [ -f "$OUTPUT_FILE" ]; then
     echo "Error: File '$OUTPUT_FILE' already exists!" >> "$COMPLETION_LOG_FILE"
+    echo "Error: File '$OUTPUT_FILE' already exists";
     exit 1
 fi
+
+# Declare from which product ID you want to start. Products with IDs greater than the FROM_ID
+# are the only ones which will be processed.
+FROM_ID=10039381622968
 
 #Let's get the total products
 echo "Total Products Request..." >> "$COMPLETION_LOG_FILE"
@@ -27,16 +32,25 @@ totalProductResponse=$(curl -X POST \
   https://${SHOPIFY_STORE}.myshopify.com/admin/api/2025-07/graphql.json \
   -H 'Content-Type: application/json' \
   -H "X-Shopify-Access-Token: ${ADMIN_API_TOKEN}" \
-  -d '{ "query": "query { productsCount(query: \"id:>=0\") { count } }" }'
+  -d '{ "query": "query { productsCount(query: \"id:>='$FROM_ID'\") { count } }" }'
 )
+
 TOTAL_PRODUCTS=$(echo $totalProductResponse | jq -r '.data.productsCount.count');
-echo "Total Products: $TOTAL_PRODUCTS" >> "$COMPLETION_LOG_FILE"
+
+echo "Total Products: $TOTAL_PRODUCTS - Starting from Product ID: $FROM_ID" >> "$COMPLETION_LOG_FILE"
 echo "-------------------------------" >> "$COMPLETION_LOG_FILE"
 echo "-------------------------------" >> "$COMPLETION_LOG_FILE"
 
 # Let's get the first product. We can use its cursor as a starting point.
-# create the GraphQL query for first product.
-QUERY_DECODED=$(generateQuery 'first:1')
+# create the GraphQL query for first product depending of the FROM_ID value.
+if [[ $FROM_ID -gt 0 ]]; then
+    SELECTOR="first:1,query:\"id:>=${FROM_ID}\""
+else
+    SELECTOR='first:1'
+fi;
+
+QUERY_DECODED=$(generateQuery $SELECTOR)
+QUERY_DECODED=$(echo "${QUERY_DECODED//\"/\\\"}")
 
 echo "First Product Request..." >> "$COMPLETION_LOG_FILE"
 firstProductQueryResponse=$(curl -X POST \
@@ -48,13 +62,26 @@ firstProductQueryResponse=$(curl -X POST \
 echo "-------------------------------" >> "$COMPLETION_LOG_FILE"
 echo "-------------------------------" >> "$COMPLETION_LOG_FILE"
 
+# Let's track the current product in the batch and the current product overall.
+CURRENT_BATCH_PRODUCT=1
+CURRENT_PRODUCT=1
+
+# The first product we'll need to retrieve manually to get a product cursor to start from.
 FIRST_PRODUCT=$(echo $firstProductQueryResponse | jq -r '.data.products.nodes[0] | @base64');
 
-$(writeProductToCsv $FIRST_PRODUCT $OUTPUT_FILE);
-
 LAST_CURSOR=$(echo $firstProductQueryResponse | jq -r '.data.products.edges[0].cursor');
-
 echo "First Product Cursor... ${LAST_CURSOR}" >> "$COMPLETION_LOG_FILE"
+
+echo "--------------------------------" >> "$COMPLETION_LOG_FILE"
+echo "-- Writing Product Image Data --" >> "$COMPLETION_LOG_FILE"
+echo "--------------------------------" >> "$COMPLETION_LOG_FILE"
+
+echo "Writing data for batch product $CURRENT_BATCH_PRODUCT / $PRODUCTS_PER_BATCH - Total Products: $CURRENT_PRODUCT / $TOTAL_PRODUCTS" >> "$COMPLETION_LOG_FILE";
+
+$(writeProductToCsv $FIRST_PRODUCT $OUTPUT_FILE $COMPLETION_LOG_FILE);
+
+let CURRENT_PRODUCT++
+let CURRENT_BATCH_PRODUCT++
 
 # How many products are we fetching per batch? Beware Shopify limits this to 250 per GraphQL query for non-bulk queries.
 PRODUCTS_PER_BATCH=250
@@ -66,21 +93,21 @@ productResponse=$(echo $(next_product_list "${LAST_CURSOR}" $PRODUCTS_PER_BATCH)
 PRODUCT_LIST=$(echo $productResponse | jq -r '.productList')
 LAST_CURSOR=$(echo $productResponse | jq -r '.lastCursor')
 
-# Order in which columnss will be written to the CSV (product ID, Handle, Image Src, Image Position)
-CURRENT_BATCH_PRODUCT=0
-CURRENT_PRODUCT=1
+# If the TOTAL_PRODUCTS is less than the PRODUCTS_PER_BATCH then set the Products Per Batch to equal
+# the total products. This will allow for the batch to finalize since there are 
+if [[ $TOTAL_PRODUCTS -lt $PRODUCTS_PER_BATCH ]]; then
+    let PRODUCTS_PER_BATCH=$TOTAL_PRODUCTS
+fi
 
+# Order in which columnss will be written to the CSV (product ID, Handle, Image Src, Image Position)
 startBatch() {
     productList=$(echo $1);
 
     # Loop through the products and save their data
     for product in $(echo "$productList" | jq -r '.[] | @base64'); do
-        echo "Writing data for batch product $CURRENT_BATCH_PRODUCT / $PRODUCTS_IN_BATCH - Total Products: $CURRENT_PRODUCT / $TOTAL_PRODUCTS" >> "$COMPLETION_LOG_FILE";
+        echo "Writing data for batch product $CURRENT_BATCH_PRODUCT / $PRODUCTS_PER_BATCH - Total Products: $CURRENT_PRODUCT / $TOTAL_PRODUCTS" >> "$COMPLETION_LOG_FILE";
 
-        $(writeProductToCsv $product $OUTPUT_FILE)
-
-        let CURRENT_PRODUCT++
-        let CURRENT_BATCH_PRODUCT++
+        $(writeProductToCsv $product $OUTPUT_FILE $COMPLETION_LOG_FILE)
 
         if [[ $CURRENT_BATCH_PRODUCT -eq $PRODUCTS_PER_BATCH ]]; then
             # Now let's generate a list of products for the first 250 products. We will user the last cursor so we know where to start from.
@@ -91,7 +118,7 @@ startBatch() {
             LAST_CURSOR=$(echo $productResponse | jq -r '.lastCursor')
 
             # Let's start the loop again to get the next product list
-            let CURRENT_BATCH_PRODUCT=0
+            let CURRENT_BATCH_PRODUCT=1
             
             echo "Batch Completed...."  >> "$COMPLETION_LOG_FILE";
             echo "-------------------------------" >> "$COMPLETION_LOG_FILE";
@@ -109,11 +136,13 @@ startBatch() {
             echo "-------------------------------" >> "$COMPLETION_LOG_FILE";
             echo "-------------------------------" >> "$COMPLETION_LOG_FILE";
         fi
+
+        let CURRENT_PRODUCT++
+        let CURRENT_BATCH_PRODUCT++
+
     done
 }
 
-echo "Next Product List Query......" >> "$COMPLETION_LOG_FILE";
-echo "Starting Cursor: $LAST_CURSOR" >> "$COMPLETION_LOG_FILE";
 $(startBatch "${PRODUCT_LIST}")
 echo "-------------------------------" >> "$COMPLETION_LOG_FILE";
 echo "-------------------------------" >> "$COMPLETION_LOG_FILE";
